97799f007b49cc90a2b48656ad12c102057923a6
Lahiru Chanaka <kodikaragelahiru@gmail.com>
2025-03-09 16:34:01 +0530

    separate calendar styles and scripts to easy access

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,9 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const calendarEl = document.getElementById("calendar");
+$(document).ready(function () {
+  const $calendarEl = $("#calendar");
 
-  const calendar = new FullCalendar.Calendar(calendarEl, {
+  const calendar = new FullCalendar.Calendar($calendarEl[0], {
     initialView: "dayGridMonth",
-    // Remove initialDate and set to current date
     headerToolbar: {
       left: "prev",
       center: "title",
@@ -15,7 +14,52 @@ document.addEventListener("DOMContentLoaded", function () {
     dayHeaderFormat: {
       weekday: "short",
     },
+    buttonText: {
+      prev: "",
+      next: "",
+    },
+    buttonHints: {
+      prev: (date) =>
+        `Previous month (${date.toLocaleString("default", { month: "long" })})`,
+      next: (date) =>
+        `Next month (${date.toLocaleString("default", { month: "long" })})`,
+    },
   });
 
   calendar.render();
+
+  const $prevButton = $calendarEl.find(".fc-prev-button");
+  const $nextButton = $calendarEl.find(".fc-next-button");
+
+  $prevButton.tooltip({
+    title: () => {
+      const date = calendar.getDate();
+      date.setMonth(date.getMonth() - 1);
+      return `Previous month (${date.toLocaleString("default", {
+        month: "long",
+      })})`;
+    },
+    placement: "bottom",
+    trigger: "hover",
+  });
+
+  $nextButton.tooltip({
+    title: () => {
+      const date = calendar.getDate();
+      date.setMonth(date.getMonth() + 1);
+      return `Next month (${date.toLocaleString("default", {
+        month: "long",
+      })})`;
+    },
+    placement: "bottom",
+    trigger: "hover",
+  });
+
+  $prevButton.on("mouseleave", function () {
+    $(this).tooltip("hide");
+  });
+
+  $nextButton.on("mouseleave", function () {
+    $(this).tooltip("hide");
+  });
 });
